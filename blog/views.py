b6a9bcefefe1fb1965b5c6892e9eef9a43d7a32a
ahmadkhan5346 <khansarfraz5346@gmail.com>
2022-07-27 20:17:45 +0530

    Creating SignUp Form

--- blog/views.py
+++ blog/views.py
@@ -11,5 +11,7 @@ def blogHome(request):
     # return HttpResponse('This is blogHome. We Will keep all the blog posts here')
 
 def blogPost(request, slug):
-    return render(request, 'blog/blogPost.html')
+    post = Post.objects.filter(slug=slug).first()
+    context = {'post':post}
+    return render(request, 'blog/blogPost.html', context)
     # return HttpResponse(f'This is blogPost:{slug}')
--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from email import message
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from home.models import Contact
 from django.contrib import messages
+from django.contrib.auth.models import User
+from blog.models import Post
 
 # Create your views here.
 
@@ -25,3 +27,47 @@ def contact(request):
             contact.save()
             messages.success(request, 'Your message has been successfuly sent')
     return render(request, 'home/contact.html')
+
+
+def search(request):
+    query = request.GET['query']
+    if len(query)>100:
+        allPosts = Post.objects.none()
+    # allPosts = Post.objects.all()
+    else:
+        allPostsTitle = Post.objects.filter(title__icontains=query)
+        allPostsContent = Post.objects.filter(content__icontains=query)
+        allPostsAuthor = Post.objects.filter(author__icontains=query)
+        allPosts = allPostsTitle.union(allPostsContent, allPostsAuthor)
+    
+    if allPosts.count() == 0:
+        messages.warning(request, 'No search results found. Please refine your query')  
+    params = {'allPosts':allPosts, 'query':query}
+    return render(request, 'home/search.html', params)
+
+
+def handleSignup(request):
+    if request.method == 'POST':
+        # Get the post parameter
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        # Check for errorneous inputs
+        # 
+
+        # Create the user
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request, 'Your Mumbai Coding Club account has been successfully created')
+        return redirect('home')
+        
+
+
+    else:
+        return HttpResponse('404 - Not Found')
